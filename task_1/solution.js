4944f632a0bcaf1cad7abcc8eff537f5324fb51d
Ivan Rybin <is.rybin@yandex.ru>
2022-04-02 12:04:30 +0300

    task 1 complete

--- task_1/solution.js
+++ task_1/solution.js
@@ -3,7 +3,12 @@ function addInCartMessage(title, price) {
     let productPrice = price;
 
     // Задание №1.1. Оповещение о добавлении в корзину
-
+    let message = productName;
+    message = message + " ";
+    message = message + "за ";
+    message = message + productPrice;
+    message = message + " теперь в корзине!";
+    console.log(message);
     // создайте переменную message
     // присвойте переменной message значение переменной productName
     // добавьте в конец переменной message пробел
@@ -23,7 +28,8 @@ function addInCartCountChange(value) {
 
     // Задание №1.2. Увеличение количества в корзине
     let oldValue = value;
-
+    let newValue = oldValue;
+    newValue = newValue + 1;
     // создайте переменную newValue
     // присвойте ей значение oldValue
     // увеличьте значение newValue на 1
@@ -36,7 +42,9 @@ function addInCartCountChange(value) {
 function addInCartSumChange(sum, delta) {
     let oldSum = sum;
     let difference = delta;
-
+    let newSum = oldSum + difference;
+    let newSumText = newSum + " ₽";
+    newSumText = `${newSum} ₽`
     // Задание №1.3. Увеличение суммы в корзине
 
     // создайте переменную newSum и присвойте ей сумму oldSum и difference
