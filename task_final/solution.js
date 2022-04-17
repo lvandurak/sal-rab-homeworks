3f7a23ebb897f0b8cf852b6bd2b28e939ad50226
Ivan Rybin <is.rybin@yandex.ru>
2022-04-17 18:34:42 +0300

    task_final done

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,20 +34,23 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
-
+    let data = {client: name + " " + phone, order: {}, goods: []};
     let countOfGoods = goods.length;
 
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
+    for (let i = 0; i <= countOfGoods - 1; i += 1) {
+        data.goods.push({title: goods[i].title, count: goods[i].count});
     }
 
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
+    data.order.address = `ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв ${address.flat}`;
+    data.order.sum = sum;
 
-    data.client = 'Иван';
-
-    let jsonData = JSON.stringify(data);
+    let jsonData = JSON.stringify({data});
 
     return jsonData;
 }
+//client: 'Иван +7(987)65-43-210'
+//client: 'Иван +7(987)65-43-210'
+//order: Object({ address: 'ул. Ленина, дом 2, 4 подъезд, 5 этаж, кв 53', sum: 900 })
+//order: Object({ address: 'ул. Ленина, дом 2, 4 подъезд, 5 этаж, кв 53', sum: 900 })
+//goods: [ Object({ title: 'Пицца', count: 2 }) ] 
+//goods: [ Object({ title: 'Пицца', count: 2 }) ]
